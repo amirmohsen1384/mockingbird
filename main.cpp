7ea93aa7f6f00462743d2bf4171203913584947b
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-05-12 00:18:13 +0330

    Updated main point

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <QApplication>
+#include "include/components/player.h"
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+
+    Player player;
+
     return app.exec();
 }
