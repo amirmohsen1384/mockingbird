f3c2e4d367d3b314e26027c8247d6de1918f0eb9
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-05-14 00:18:59 +0330

    Implemented some features in the player
    Defined the ability to represent a random or sequential play.

--- include/components/player.h
+++ include/components/player.h
@@ -12,23 +12,24 @@ class Player;
 class Player : public QWidget
 {
     Q_OBJECT
-protected:
-    void closeEvent(QCloseEvent *event) override;
 
 public:
+    Q_DISABLE_COPY_MOVE(Player)
     explicit Player(QWidget *parent = nullptr);
     ~Player();
     
     QUrl source() const;
+    bool isShuffleMode() const;
+    bool isInfiniteMode() const;
 
-public slots:
-    void updateLoop();
+private slots:
     void updateMetaData();
     void updateProgress();
     void updatePlayback();
+    void updateShuffleButton();
     void updatePlaybackSpeed();
     void updateSeekingControl();
-    void setSource(const QUrl &source);
+    void updateInfiniteButton();
 
 public slots:
     void stop();
@@ -36,6 +37,14 @@ public slots:
     void forward();
     void controlPlayback();
     void controlPlaybackSpeed();
+    void setShuffleMode(bool value);
+    void setInfiniteMode(bool value);
+    void setSource(const QUrl &source);
+
+signals:
+    void sourceFinished();
+    void shuffleModeChanged(bool value);
+    void infiniteModeChanged(bool value);
 
 private:
     Ui::Player *ui;
