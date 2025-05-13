cbc38036391d60676f801f6c832a05fb7ffded13
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-05-13 14:16:18 +0330

    Defined a song

---
+++ include/core/song.h
@@ -0,0 +1,62 @@
+#ifndef SONG_H
+#define SONG_H
+
+#include <QObject>
+
+class Song : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Song(QObject *parent = nullptr);
+    Song(const Song &other, QObject *parent = nullptr);
+    Song(Song &&other, QObject *parent = nullptr) noexcept;
+
+    Song& operator=(const Song &other);
+    Song& operator=(Song&& other);
+
+    QUrl getAddress() const;
+    QString getName() const;
+    QString getGenre() const;
+    QString getArtist() const;
+    int getPublicationYear() const;
+    
+    friend bool operator==(const Song &one, const Song &two);
+    friend bool operator!=(const Song &one, const Song &two);
+
+    friend QDebug operator<<(QDebug debug, const Song &song);
+    friend QDataStream& operator<<(QDataStream &stream, const Song &another);
+    friend QDataStream& operator>>(QDataStream &stream, Song &another);
+
+public slots:
+    void setName(const QString &value);
+    void setGenre(const QString &value);
+    void setArtist(const QString &value);
+    void setPublicationYear(int value);
+    void setAddress(const QUrl &value);
+
+signals:
+    void nameChanged(QString name);
+    void genreChanged(QString genre);
+    void addressChanged(QUrl address);
+    void artistChanged(QString artist);
+    void publicationYearChanged(int year);
+
+private:
+    QUrl address;
+    QString name;
+    QString genre;
+    QString artist;
+    int publicationYear;
+};
+
+bool operator==(const Song &one, const Song &two);
+bool operator!=(const Song &one, const Song &two);
+
+QDataStream& operator<<(QDataStream &stream, const Song &another);
+QDataStream& operator>>(QDataStream &stream, Song &another);
+
+Q_DECLARE_METATYPE(Song)
+
+using SongList = QList<Song>;
+
+#endif // SONG_H
---
+++ src/core/song.cpp
@@ -0,0 +1,151 @@
+#include "include/core/song.h"
+#include <QDataStream>
+#include <QDebug>
+#include <QUrl>
+
+Song::Song(QObject *parent) : QObject{parent} {}
+
+Song& Song::operator=(const Song &another)
+{
+    name = another.name;
+    genre = another.genre;
+    artist = another.artist;
+    address = another.address;
+    publicationYear = another.publicationYear;
+    return *this;
+}
+
+Song& Song::operator=(Song &&another)
+{
+    name = std::move(another.name);
+    genre = std::move(another.genre);
+    artist = std::move(another.artist);
+    address = std::move(another.address);
+    publicationYear = std::move(another.publicationYear);
+    return *this;
+}
+
+Song::Song(const Song &other, QObject *parent) : Song(parent)
+{
+    *this = other;
+}
+Song::Song(Song &&other, QObject *parent) noexcept : Song(parent)
+{
+    *this = std::move(other);
+}
+
+bool operator==(const Song &one, const Song &two)
+{
+    if(one.name != two.name) {
+        return false;
+    }
+    else if(one.genre != two.genre) {
+        return false;
+    }
+    else if(one.artist != two.artist) {
+        return false;
+    }
+    else if(one.publicationYear != two.publicationYear) {
+        return false;
+    }
+    else if(one.address != two.address) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+bool operator!=(const Song &one, const Song &two)
+{
+    return !(one == two);
+}
+
+QString Song::getName() const
+{
+    return name;
+}
+QString Song::getGenre() const
+{
+    return genre;
+}
+QString Song::getArtist() const
+{
+    return artist;
+}
+int Song::getPublicationYear() const
+{
+    return publicationYear;
+}
+QUrl Song::getAddress() const
+{
+    return address;
+}
+void Song::setName(const QString &value)
+{
+    if (name != value) {
+        name = value;
+        emit nameChanged(name);
+    }
+}
+void Song::setGenre(const QString &value)
+{
+    if (genre != value) {
+        genre = value;
+        emit genreChanged(genre);
+    }
+}
+void Song::setArtist(const QString &value)
+{
+    if (artist != value) {
+        artist = value;
+        emit artistChanged(artist);
+    }
+}
+void Song::setPublicationYear(int value)
+{
+    if (publicationYear != value) {
+        publicationYear = value;
+        emit publicationYearChanged(publicationYear);
+    }
+}
+void Song::setAddress(const QUrl &value)
+{
+    if (address != value) {
+        address = value;
+        emit addressChanged(address);
+    }
+}
+
+QDataStream& operator<<(QDataStream &stream, const Song &another)
+{
+    stream << another.name;
+    stream << another.genre;
+    stream << another.artist;
+    stream << another.publicationYear;
+    stream << another.address;
+    return stream;
+}
+
+QDataStream& operator>>(QDataStream &stream, Song &another)
+{
+    stream >> another.name;
+    stream >> another.genre;
+    stream >> another.artist;
+    stream >> another.publicationYear;
+    stream >> another.address;
+    return stream;
+}
+
+QDebug operator<<(QDebug debug, const Song &song)
+{
+    QDebugStateSaver saver(debug);
+    debug.nospace() << "Song(";
+    debug.nospace() << "name: " << song.getName() << ", ";
+    debug.nospace() << "genre: " << song.getGenre() << ", ";
+    debug.nospace() << "artist: " << song.getArtist() << ", ";
+    debug.nospace() << "publicationYear: " << song.getPublicationYear() << ", ";
+    debug.nospace() << "address: " << song.getAddress();
+    debug.nospace() << ")";
+    return debug;
+}
