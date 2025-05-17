d8b8bdfa25f25c823b9fc1b03b9b652330eb08d1
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-05-18 02:09:42 +0330

    Defined a song edit
    This editor will be used to edit the information of a song.

---
+++ include/components/songedit.h
@@ -0,0 +1,67 @@
+#ifndef SONGEDIT_H
+#define SONGEDIT_H
+
+#include <QWidget>
+#include <QMediaPlayer>
+#include "include/models/genremodel.h"
+
+namespace Ui
+{
+    class SongEdit;
+}
+class QFileDialog;
+
+class SongEdit : public QWidget
+{
+    Q_OBJECT
+private:
+    QFileDialog* browseFile();
+
+private slots:
+    void updateSource();
+    void openSongFile();
+    void openImageFile();
+    void updateMetaData();
+    void controlPlayback();
+    void updatePlaybackControl(bool value);
+
+public:
+    Q_DISABLE_COPY_MOVE(SongEdit)
+    explicit SongEdit(QWidget *parent = nullptr);
+    explicit SongEdit(const Song &value, QWidget *parent = nullptr);
+
+    ~SongEdit();
+
+    Song::Genre getGenre() const;
+    int getReleasedYear() const;
+    QString getArtist() const;
+    QUrl getLocation() const;
+    QPixmap getCover() const;
+    QString getName() const;
+    Song getValue() const;
+
+public slots:
+    void setReleasedYear(int value);
+    void setValue(const Song &value);
+    void setGenre(Song::Genre value);
+    void setName(const QString &value);
+    void setCover(const QPixmap &value);
+    void setArtist(const QString &value);
+    void setLocation(const QUrl &location);
+
+signals:
+    void nameChanged(QString value);
+    void locationChanged(QUrl value);
+    void coverChanged(QPixmap value);
+    void artistChanged(QString value);
+    void releasedYearChanged(int value);
+    void genreChanged(Song::Genre value);
+
+private:
+    GenreModel model;
+    std::unique_ptr<Ui::SongEdit> ui;
+    std::unique_ptr<QAudioOutput> output;
+    std::unique_ptr<QMediaPlayer> player;
+};
+
+#endif // SONGEDIT_H
---
+++ src/components/songedit.cpp
@@ -0,0 +1,237 @@
+#include "include/components/songedit.h"
+#include <QMediaMetaData>
+#include <QMediaDevices>
+#include "ui_songedit.h"
+#include <QAudioOutput>
+#include <QAudioDevice>
+#include <QFileDialog>
+
+SongEdit::SongEdit(QWidget *parent) : QWidget{parent}
+{
+    ui = std::make_unique<Ui::SongEdit>();
+    ui->setupUi(this);
+
+    output = std::make_unique<QAudioOutput>();
+    output->setDevice(QMediaDevices::defaultAudioOutput());
+
+    player = std::make_unique<QMediaPlayer>();
+    player->setAudioOutput(output.get());
+
+    ui->genreEdit->setModel(&model);
+    ui->playbackButton->setVisible(false);
+    ui->releaseYearEdit->setValue(QDate::currentDate().year());
+
+    connect(this, &SongEdit::locationChanged, player.get(), &QMediaPlayer::setSource);
+    connect(player.get(), &QMediaPlayer::sourceChanged, this, &SongEdit::updateSource);
+    connect(player.get(), &QMediaPlayer::metaDataChanged, this, &SongEdit::updateMetaData);
+    connect(player.get(), &QMediaPlayer::playingChanged, this, &SongEdit::updatePlaybackControl);
+
+    connect(ui->nameEdit, &QLineEdit::textChanged, this, &SongEdit::nameChanged);
+    connect(ui->artistEdit, &QLineEdit::textChanged, this, &SongEdit::artistChanged);
+    connect(ui->fileNameEdit, &QLineEdit::textChanged, this, [&](const QString &text)
+    {
+        emit locationChanged(QUrl::fromLocalFile(text));
+    }
+    );
+    connect(ui->genreEdit, &QComboBox::currentIndexChanged, this, [&](int index)
+    {
+        emit genreChanged(static_cast<Song::Genre>(index));
+    }
+    );
+    connect(ui->coverView, &ImageView::imageChanged, this, [&](const QImage &image)
+    {
+        emit SongEdit::coverChanged(QPixmap::fromImage(image));
+    }
+    );
+    connect(ui->releaseYearEdit, &QSpinBox::valueChanged, this, &SongEdit::releasedYearChanged);
+}
+
+SongEdit::SongEdit(const Song &value, QWidget *parent) : SongEdit(parent)
+{
+    setValue(value);
+}
+
+QFileDialog *SongEdit::browseFile()
+{
+    QFileDialog *dialog = new QFileDialog(this);
+    dialog->setDirectory(QDir::home());
+    dialog->setAcceptMode(QFileDialog::AcceptOpen);
+    dialog->setFileMode(QFileDialog::ExistingFile);
+    dialog->setWindowTitle("Select a file to open");
+    dialog->setFilter(QDir::AllEntries | QDir::NoDotAndDotDot);
+    return dialog;
+}
+
+void SongEdit::updateSource()
+{
+    player->stop();
+    ui->playbackButton->setVisible(player->mediaStatus() != QMediaPlayer::InvalidMedia);
+}
+
+void SongEdit::openSongFile()
+{
+    static QStringList history;
+    std::unique_ptr<QFileDialog> dialog(browseFile());
+    dialog->setNameFilter("Songs (*.mp3 *.wav *.aiff *.pcm *.aac *.ogg *.wma *.m4a *.ape *.alac)");
+    dialog->setHistory(history);
+    if(dialog->exec() == QDialog::Accepted)
+    {
+        const QString fileName = dialog->selectedFiles().constFirst();
+        setLocation(QUrl::fromLocalFile(fileName));
+        history.append(fileName);
+    }
+}
+
+void SongEdit::openImageFile()
+{
+    static QStringList history;
+    std::unique_ptr<QFileDialog> dialog(browseFile());
+    dialog->setNameFilter("Pictures (*.bmp *.png *.tiff *.gif *.jpeg *.jpg *.tif)");
+    dialog->setHistory(history);
+    if(dialog->exec() == QDialog::Accepted)
+    {
+        const QString fileName = dialog->selectedFiles().constFirst();
+        setCover(QPixmap(fileName));
+        history.append(fileName);
+    }
+}
+
+void SongEdit::updateMetaData()
+{
+    // Fetches the meta data from the media player
+    QMediaMetaData data = player->metaData();
+
+    // Initializes the name
+    QVariant name = data.value(QMediaMetaData::Title);
+    if(name.isNull())
+    {
+        name = data.value(QMediaMetaData::AlbumTitle);
+        if(name.isNull())
+        {
+            name = data.value(QMediaMetaData::Description);
+        }
+    }
+    setName(name.toString());
+
+    // Initializes the image cover
+    QVariant cover = data.value(QMediaMetaData::ThumbnailImage);
+    if(cover.isNull())
+    {
+        cover = data.value(QMediaMetaData::CoverArtImage);
+    }
+    setCover(qvariant_cast<QPixmap>(cover));
+
+    // Initializes the artist
+    QVariant artist = data.value(QMediaMetaData::AlbumArtist);
+    if(artist.isNull())
+    {
+        artist = data.value(QMediaMetaData::Author);
+        if(artist.isNull())
+        {
+            artist = data.value(QMediaMetaData::ContributingArtist);
+        }
+    }
+    setArtist(artist.toString());
+}
+
+void SongEdit::controlPlayback()
+{
+    if(player->isPlaying())
+    {
+        player->pause();
+    }
+    else
+    {
+        player->play();
+    }
+}
+
+void SongEdit::updatePlaybackControl(bool value)
+{
+    ui->playbackButton->setToolTip(value ? "Pause" : "Play");
+    ui->playbackButton->setIcon(QIcon(QString(":/images/player/%1.png").arg(value ? "pause" : "play")));
+}
+
+SongEdit::~SongEdit() {}
+
+Song::Genre SongEdit::getGenre() const
+{
+    return static_cast<Song::Genre>(ui->genreEdit->currentIndex());
+}
+
+int SongEdit::getReleasedYear() const
+{
+    return ui->releaseYearEdit->value();
+}
+
+QString SongEdit::getArtist() const
+{
+    return ui->artistEdit->text();
+}
+
+QUrl SongEdit::getLocation() const
+{
+    return QUrl::fromLocalFile(ui->fileNameEdit->text());
+}
+
+QPixmap SongEdit::getCover() const
+{
+    return QPixmap::fromImage(ui->coverView->getImage());
+}
+
+QString SongEdit::getName() const
+{
+    return ui->nameEdit->text();
+}
+
+Song SongEdit::getValue() const
+{
+    Song target;
+    target.setName(getName());
+    target.setCover(getCover());
+    target.setGenre(getGenre());
+    target.setArtist(getArtist());
+    target.setAddress(getLocation());
+    target.setPublicationYear(getReleasedYear());
+    return target;
+}
+
+void SongEdit::setReleasedYear(int value)
+{
+    ui->releaseYearEdit->setValue(value);
+}
+
+void SongEdit::setValue(const Song &value)
+{
+    setName(value.name);
+    setGenre(value.genre);
+    setCover(value.cover);
+    setArtist(value.artist);
+    setLocation(value.address);
+    setReleasedYear(value.publicationYear);
+}
+
+void SongEdit::setGenre(Song::Genre value)
+{
+    ui->genreEdit->setCurrentIndex(static_cast<int>(value));
+}
+
+void SongEdit::setName(const QString &value)
+{
+    ui->nameEdit->setText(value);
+}
+
+void SongEdit::setCover(const QPixmap &value)
+{
+    ui->coverView->setImage(value.toImage());
+}
+
+void SongEdit::setArtist(const QString &value)
+{
+    ui->artistEdit->setText(value);
+}
+
+void SongEdit::setLocation(const QUrl &location)
+{
+    ui->fileNameEdit->setText(location.toLocalFile());
+}
