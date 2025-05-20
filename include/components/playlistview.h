e9c5b47f51e9e893e589a59d8694e9e478177f71
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-05-20 21:33:02 +0330

    Defined the playlist view
    This widget provides read-only access to the songs of a playlist.

---
+++ include/components/playlistview.h
@@ -0,0 +1,39 @@
+#ifndef PLAYLISTVIEW_H
+#define PLAYLISTVIEW_H
+
+#include <QWidget>
+#include "include/models/proxyplaylistmodel.h"
+#include "include/components/custom/filteredit.h"
+
+namespace Ui {
+class PlaylistView;
+}
+
+class PlaylistView : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit PlaylistView(QWidget *parent = nullptr);
+    ~PlaylistView();
+
+    std::shared_ptr<ProxyPlaylistModel> getModel();
+    void setModel(std::shared_ptr<ProxyPlaylistModel> &model);
+
+public slots:
+    void showArrangePanel(bool toggle);
+    void showFilterEdit(bool toggle);
+    void showFindPanel(bool toggle);
+
+private slots:
+    void updateArrangeCriteria();
+    void updateFindCriteria();
+    void updateFilter();
+    void updateModel();
+
+private:
+    std::unique_ptr<Ui::PlaylistView> ui;
+    std::shared_ptr<ProxyPlaylistModel> model;
+};
+
+#endif // PLAYLISTVIEW_H
---
+++ src/components/playlistview.cpp
@@ -0,0 +1,140 @@
+#include "include/components/playlistview.h"
+#include "ui_playlistview.h"
+
+PlaylistView::PlaylistView(QWidget *parent) : QWidget(parent)
+{
+    ui = std::make_unique<Ui::PlaylistView>();
+    ui->setupUi(this);
+
+    ui->arrangePanel->setVisible(false);
+    ui->filterEdit->setVisible(false);
+    ui->findPanel->setVisible(false);
+
+    connect(ui->filterEdit, &FilterEdit::filterChanged, this, &PlaylistView::updateFilter);
+    connect(ui->findPanel, &FindWidget::findPropertyChanged, this, &PlaylistView::updateFindCriteria);
+    connect(ui->arrangePanel, &ArrangeWidget::sortCriteriaChanged, this, &PlaylistView::updateArrangeCriteria);
+}
+
+PlaylistView::~PlaylistView()
+{}
+
+std::shared_ptr<ProxyPlaylistModel> PlaylistView::getModel()
+{
+    return model;
+}
+
+void PlaylistView::setModel(std::shared_ptr<ProxyPlaylistModel> &model)
+{
+    this->model = model;
+    updateModel();
+}
+
+void PlaylistView::showArrangePanel(bool toggle)
+{
+    if(toggle)
+    {
+        ui->arrangePanel->setVisible(true);
+        ui->findPanel->setVisible(false);
+        ui->filterEdit->setVisible(false);
+
+        ui->findButton->setChecked(false);
+        ui->filterButton->setChecked(false);
+    }
+    else
+    {
+        ui->arrangePanel->setVisible(false);
+    }
+}
+
+void PlaylistView::showFilterEdit(bool toggle)
+{
+    if(toggle)
+    {
+        ui->filterEdit->setVisible(true);
+        ui->findPanel->setVisible(false);
+        ui->arrangePanel->setVisible(false);
+
+        ui->findButton->setChecked(false);
+        ui->arrangeButton->setChecked(false);
+    }
+    else
+    {
+        ui->filterEdit->setVisible(false);
+    }
+
+}
+
+void PlaylistView::showFindPanel(bool toggle)
+{
+    if(toggle)
+    {
+        ui->findPanel->setVisible(true);
+        ui->filterEdit->setVisible(false);
+        ui->arrangePanel->setVisible(false);
+
+        ui->filterButton->setChecked(false);
+        ui->arrangeButton->setChecked(false);
+    }
+    else
+    {
+        ui->findPanel->setVisible(false);
+    }
+}
+
+void PlaylistView::updateArrangeCriteria()
+{
+    if(model.get() != nullptr)
+    {
+        model->setSortRole(static_cast<int>(ui->arrangePanel->getArrangeBase()));
+    }
+}
+
+void PlaylistView::updateFindCriteria()
+{
+    if(model.get() != nullptr)
+    {
+        QRegularExpression target;
+        Qt::MatchFlags flags = ui->findPanel->getFlags();
+        if(flags.testFlag(Qt::MatchFlag::MatchStartsWith))
+        {
+            target.setPattern(QString("^%1").arg(ui->findPanel->getText()));
+        }
+        else if(flags.testFlag(Qt::MatchFlag::MatchEndsWith))
+        {
+            target.setPattern(QString("%1$").arg(ui->findPanel->getText()));
+        }
+        else if(flags.testFlag(Qt::MatchFlag::MatchContains))
+        {
+            target.setPattern(QString("%1").arg(ui->findPanel->getText()));
+        }
+        else if(flags.testFlag(Qt::MatchFlag::MatchFixedString))
+        {
+            target.setPattern(QString("%1").arg(ui->findPanel->getText()));
+        }
+        model->setFilterRegularExpression(target);
+        model->setFilterRole(static_cast<int>(ui->findPanel->getSearchBase()));
+        model->setFilterCaseSensitivity(ui->findPanel->getCaseSensitivityMode());
+    }
+}
+
+void PlaylistView::updateFilter()
+{
+    if(model.get() != nullptr)
+    {
+        model->setFilteringGenre(ui->filterEdit->getGenre());
+        model->setMinimumYear(ui->filterEdit->getMinimumYear());
+        model->setMaximumYear(ui->filterEdit->getMaximumYear());
+    }
+}
+
+void PlaylistView::updateModel()
+{
+    PlaylistModel *original = dynamic_cast<PlaylistModel*>(model.get());
+    ui->songView->setModel(model.get());
+    if(original != nullptr)
+    {
+        updateFilter();
+        updateFindCriteria();
+        updateArrangeCriteria();
+    }
+}
