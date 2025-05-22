b2d59d5bdecbb6cfcc25860d1b78797e251840b2
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-05-22 15:01:49 +0330

    Testing the program

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
+#include <QDir>
 #include <QApplication>
 #include "include/dialogs/songedit.h"
 #include "include/windows/welcomewindow.h"
+#include "playlistedit.h"
+#include "include/models/playlistmodel.h"
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
 
-    SongEdit window;
-    window.show();
+    auto songs = QDir("C:/Users/Amir Mohsen/Downloads/Music").entryInfoList({"*.mp3"}, QDir::AllEntries | QDir::NoDotAndDotDot);
+    std::shared_ptr<PlaylistModel> model = std::make_shared<PlaylistModel>();
+    for(const QFileInfo &i : songs)
+    {
+        Song target;
+        target.setAddress(i.absoluteFilePath());
+        target.setName(i.baseName());
+        model->appendSong(target);
+    }
 
-    app.exec();
+    PlaylistEdit editor;
+    editor.setModel(model);
+    editor.show();
 
-    return 0;
+    return app.exec();
 }
