56dd930e57cea2f4cdfea6b61626f5eb3bacc14d
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-05-24 17:12:35 +0330

    Completed the artist editor

--- include/dialogs/artistedit.h
+++ include/dialogs/artistedit.h
@@ -2,6 +2,8 @@
 #define ARTISTEDIT_H
 
 #include <QDialog>
+#include <QAbstractItemModel>
+#include "include/models/artistmodel.h"
 
 namespace Ui {
 class ArtistEdit;
@@ -10,13 +12,28 @@ class ArtistEdit;
 class ArtistEdit : public QDialog
 {
     Q_OBJECT
+private slots:
+    void updateMetaData();
 
 public:
+    Q_DISABLE_COPY_MOVE(ArtistEdit)
+    explicit ArtistEdit(const Artist &value, QWidget *parent = nullptr);
     explicit ArtistEdit(QWidget *parent = nullptr);
     ~ArtistEdit();
 
+    Artist artist() const;
+    void setArtist(const Artist &value);
+
+public slots:
+    void addPlaylist();
+    void removePlaylist();
+    void editPlaylist(const QModelIndex &index);
+
+    virtual void accept() override;
+
 private:
-    Ui::ArtistEdit *ui;
+    std::unique_ptr<Ui::ArtistEdit> ui;
+    std::unique_ptr<ArtistModel> container;
 };
 
 #endif // ARTISTEDIT_H
--- src/dialogs/artistedit.cpp
+++ src/dialogs/artistedit.cpp
@@ -1,14 +1,122 @@
+#include "include/dialogs/playlistedit.h"
 #include "include/dialogs/artistedit.h"
 #include "ui_artistedit.h"
+#include <QMessageBox>
 
-ArtistEdit::ArtistEdit(QWidget *parent)
-    : QDialog(parent)
-    , ui(new Ui::ArtistEdit)
+void ArtistEdit::updateMetaData()
 {
-    ui->setupUi(this);
+    const Artist &artist = container->artist();
+    ui->nameEdit->setText(artist.getName());
+    ui->photoView->setImage(artist.getPhoto().toImage());
+    ui->biographyEdit->setPlainText(artist.getBiography());
+    setWindowTitle(QString("%1 - Artist Editor").arg(artist.getName()));
 }
 
-ArtistEdit::~ArtistEdit()
+ArtistEdit::ArtistEdit(const Artist &value, QWidget *parent) : ArtistEdit(parent)
 {
-    delete ui;
+    setArtist(value);
+}
+
+ArtistEdit::ArtistEdit(QWidget *parent) : QDialog(parent)
+{
+    ui = std::make_unique<Ui::ArtistEdit>();
+    ui->setupUi(this);
+
+    ui->removeButton->setVisible(false);
+
+    container = std::make_unique<ArtistModel>();
+    ui->playlistView->setModel(container.get());
+
+    connect(ui->playlistView->selectionModel(), &QItemSelectionModel::selectionChanged, this,
+        [&](const QItemSelection &selected, const QItemSelection &deselected)
+        {
+            Q_UNUSED(selected)
+            Q_UNUSED(deselected)
+            auto indexes = ui->playlistView->selectionModel()->selectedIndexes();
+            ui->removeButton->setVisible(!indexes.isEmpty());
+        }
+    );
+}
+
+ArtistEdit::~ArtistEdit() {}
+
+Artist ArtistEdit::artist() const
+{
+    return container->artist();
+}
+
+void ArtistEdit::setArtist(const Artist &value)
+{
+    container->setArtist(value);
+    updateMetaData();
+}
+
+void ArtistEdit::addPlaylist()
+{
+    PlaylistEdit editor;
+    if(editor.exec() == QDialog::Accepted)
+    {
+        container->appendPlaylist(editor.playlist());
+    }
+}
+
+void ArtistEdit::removePlaylist()
+{
+    auto indices = ui->playlistView->selectionModel()->selectedIndexes();
+
+    QMessageBox message;
+    message.setWindowTitle("Confirm");
+    message.setIcon(QMessageBox::Warning);
+    message.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+    message.setText(QString("Are you sure to remove %1 playlist(s) permanently?").arg(indices.size()));
+
+    if(message.exec() == QMessageBox::Yes)
+    {
+        std::sort(indices.begin(), indices.end(),
+            [&](const QModelIndex &one, const QModelIndex &two)
+            {
+                return one.row() >= two.row();
+            }
+        );
+
+        for(const QModelIndex &index : indices)
+        {
+            container->removePlaylist(index.row());
+        }
+    }
+}
+
+void ArtistEdit::editPlaylist(const QModelIndex &index)
+{
+    Playlist data = index.data(Qt::UserRole).value<Playlist>();
+    PlaylistEdit editor(data);
+    if(editor.exec() == QDialog::Accepted)
+    {
+        container->setData(index, QVariant::fromValue(editor.playlist()), Qt::UserRole);
+        qDebug() << "Hello";
+    }
+}
+
+void ArtistEdit::accept()
+{
+    try
+    {
+        if(ui->nameEdit->text().isEmpty())
+        {
+            throw std::runtime_error("You have not entered the name.");
+        }
+        else if(ui->biographyEdit->toPlainText().isEmpty())
+        {
+            throw std::runtime_error("You have not entered the biography.");
+        }
+        container->setName(ui->nameEdit->text());
+        container->setBiography(ui->biographyEdit->toPlainText());
+        container->setPhoto(QPixmap::fromImage(ui->photoView->getImage()));
+    }
+    catch(std::exception &e)
+    {
+        QMessageBox::critical(this, "Error", e.what());
+        return;
+    }
+    QDialog::accept();
 }
