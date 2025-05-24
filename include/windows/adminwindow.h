8c111276feb5ef10783d20a64253ae2bf71657fb
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-05-25 00:44:30 +0330

    Implemented the meta data section of the page

--- include/windows/adminwindow.h
+++ include/windows/adminwindow.h
@@ -13,18 +13,26 @@ class AdminWindow : public QMainWindow
     Q_OBJECT
 public:
     explicit AdminWindow(const Admin &admin, QWidget *parent = nullptr);
+    explicit AdminWindow(QWidget *parent = nullptr);
     ~AdminWindow();
 
     Admin adminstrator() const;
 
+private slots:
+    void updateMetaData();
+    void rejectChanging();
+    void acceptChanging();
+    void togglePasswordShow(bool toggle);
+
 public slots:
     void setAdminstrator(const Admin &value);
+    void goToInfoChangingMode();
 
 signals:
     void adminstratorChanged(const Admin &value);
 
 private:
-    Admin adminstrator;
+    Admin admin;
     std::unique_ptr<Ui::AdminWindow> ui;
 };
 
--- src/windows/adminwindow.cpp
+++ src/windows/adminwindow.cpp
@@ -1,5 +1,12 @@
 #include "include/windows/adminwindow.h"
 #include "ui_adminwindow.h"
+#include <QMessageBox>
+#include <exception>
+
+AdminWindow::AdminWindow(const Admin &admin, QWidget *parent) : AdminWindow(parent)
+{
+    setAdminstrator(admin);
+}
 
 AdminWindow::AdminWindow(QWidget *parent) : QMainWindow(parent)
 {
@@ -11,11 +18,95 @@ AdminWindow::~AdminWindow() {}
 
 Admin AdminWindow::adminstrator() const
 {
-    return adminstrator;
+    return admin;
+}
+
+void AdminWindow::updateMetaData()
+{
+    ui->adminNameLabel->setText(admin.getFullName());
+    ui->firstNameEdit->setText(admin.getFirstName());
+    ui->lastNameEdit->setText(admin.getLastName());
+    ui->userNameEdit->setText(admin.getUserName());
+    ui->passwordEdit->setText(admin.getPassword());
 }
 
 void AdminWindow::setAdminstrator(const Admin &value)
 {
-    adminstrator = value;
+    admin = value;
+    updateMetaData();
     emit adminstratorChanged(value);
+}
+
+void AdminWindow::rejectChanging()
+{
+    ui->pageContainer->setCurrentWidget(ui->mainPage);
+    updateMetaData();
+}
+
+void AdminWindow::acceptChanging()
+{
+    try
+    {
+        const QString firstName = ui->firstNameEdit->text();
+        const QString lastName = ui->lastNameEdit->text();
+        const QString userName = ui->userNameEdit->text();
+        const QString password = ui->passwordEdit->text();
+
+        QRegularExpression pattern("[a-zA-Z ]{1,}");
+        if(!pattern.match(firstName).hasMatch())
+        {
+            throw std::runtime_error("The first name should only contain non-letter characters and not be empty.");
+        }
+        else if(!pattern.match(lastName).hasMatch())
+        {
+            throw std::runtime_error("The last name should only contain non-letter characters and not be empty.");
+        }
+
+        pattern.setPattern(".{6,}");
+        if(!pattern.match(userName).hasMatch())
+        {
+            throw std::runtime_error("The user name should have at least 6 characters.");
+        }
+
+        pattern.setPattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_])[A-Za-z\\d\\W_]{8,}$");
+        if(!pattern.match(password).hasMatch())
+        {
+            throw std::runtime_error("A strong password is at least 8 character, and has all kinds of characters.");
+        }
+
+        admin.setFirstName(firstName);
+        admin.setLastName(lastName);
+        admin.setUserName(userName);
+        admin.setPassword(password);
+    }
+    catch(std::exception &e)
+    {
+        QMessageBox::critical(this, "Error", e.what());
+        return;
+    }
+
+    if(admin.saveToRecord())
+    {
+        updateMetaData();
+    }
+    ui->pageContainer->setCurrentWidget(ui->mainPage);
+}
+
+void AdminWindow::togglePasswordShow(bool toggle)
+{
+    if(!toggle)
+    {
+        ui->passwordEdit->setEchoMode(QLineEdit::EchoMode::Password);
+        ui->showPasswordButton->setIcon(QIcon(":/images/windows/show-password.png"));
+    }
+    else
+    {
+        ui->passwordEdit->setEchoMode(QLineEdit::Normal);
+        ui->showPasswordButton->setIcon(QIcon(":/images/windows/hide-password.png"));
+    }
+}
+
+void AdminWindow::goToInfoChangingMode()
+{
+    ui->pageContainer->setCurrentWidget(ui->informationPage);
 }
